4b3f679a7c1ff28486a3813253a655c0739cd8f3
Samuel <sayag.samuel@live.com>
2017-10-30 21:26:37 +0200

    fix git + fix js

--- src/test/samuel/blog/wwwroot/js/site.js
+++ src/test/samuel/blog/wwwroot/js/site.js
@@ -1,6 +1,18 @@
 ﻿// site js
 
 (function (){
-    
+    // var ele = $("#username");
+
+    // ele.text("new user name");
+
+    var $sidebarAndWarrper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWarrper.toggleClass("hide-sidebar");
+        if($sidebarAndWarrper.hasClass("hide-sidebar")) {
+            $(this).text("Show");
+        } else {
+            $(this).text("Hide");
+        } 
+    });
 
 })();
