5e610b5b8ca9a5e00d9ef6c69e96e10cc84167e1
Samuel <sayag.samuel@live.com>
2017-10-30 23:51:01 +0200

    adding MVC suport and some views: about, contact and main

---
+++ src/test/samuel/blog/ViewModels/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace blog.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(4096, MinimumLength = 10 )]
+        public string Message { get; set; }
+
+    }
+}
