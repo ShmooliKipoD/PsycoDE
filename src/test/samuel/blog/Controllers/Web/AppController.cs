61d744c9fdeec57a426764bc7a9567239f510ded
Sayag <Samuel.Sayag@kla-tencor.com>
2017-10-31 13:16:34 +0200

    Adding post support to contact page.

--- src/test/samuel/blog/Controllers/Web/AppController.cs
+++ src/test/samuel/blog/Controllers/Web/AppController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -19,6 +20,12 @@ namespace blog.Controllers.Web
         public IActionResult About()
         {
             //throw new Exception("bad things");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel vm){
 
             return View();
         }
